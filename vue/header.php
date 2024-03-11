0dd7a6ea7d84885bffe4bba6c979d323f1849c50
Tomy Rulliat <tomy.rulliat@clsi.fr>
2024-03-11 17:01:02 +0100

    title en dr

--- vue/header.php
+++ vue/header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title><?= $titre ?></title>
+	<title>DCS IT Games</title>
 	
 
 	<link href="bib/bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
--- vue/v_accueil.php
+++ vue/v_accueil.php
@@ -1,3 +1,5 @@
 <h1>
 Bienvenue sur le tableau de bord.
 </h1>
+<br><br>
+<h2></h2>
