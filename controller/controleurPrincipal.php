0c52ce5a47305857898566af60e780acf91b1990
ZYXALAG <adrien.galaxyz@gmail.com>
2024-03-11 14:20:35 +0100

    Remove default action and set accueil as default action

--- controller/controleurPrincipal.php
+++ controller/controleurPrincipal.php
@@ -4,7 +4,6 @@ function controleurPrincipal($action) {
     $lesActions["accueil"] = "c_accueil.php";
     $lesActions["connexion"] = "c_connexion.php";
     $lesActions["inscription"] = "c_inscription.php";
-    $lesActions["defaut"] = $lesActions["accueil"];
     $lesActions["deconnexion"] = "c_deconnexion.php";
     $lesActions["modifMdp"] = "c_modification_mdp.php";
     
@@ -12,7 +11,7 @@ function controleurPrincipal($action) {
     if (array_key_exists($action, $lesActions)) {
         return $lesActions[$action];
     } else {
-        return $lesActions["defaut"];
+        return $lesActions["accueil"];
     }
 }
 
