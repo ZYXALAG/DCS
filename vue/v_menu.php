976960e828b11602f58f68b72d3ca4b2b7178f59
Warous <arous.wissem34@gmail.com>
2024-03-11 16:59:42 +0100

    Finition 2ème partie Ecran 1

--- vue/v_menu.php
+++ vue/v_menu.php
@@ -6,7 +6,7 @@
           <a class="nav-link" href="./?action=accueil">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./?action=appligc">Top des coûts d'application par GrandClient</a>
+          <a class="nav-link" href="./?action=appligc">Top des coûts d'application par Grand Clients</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./?action=evolutionMensuelle">Evolution Mensuelle</a>
