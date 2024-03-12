3cd2de4e3f1ac9b6992323666b6fa54575201291
ZYXALAG <adrien.galaxyz@gmail.com>
2024-03-12 10:48:06 +0100

    Update database configuration

--- infoBDD.inc.php
+++ infoBDD.inc.php
@@ -2,8 +2,8 @@
 /**
  * Fichier de configuration de la base de données
  */
-    $_ENV["username"] = "root"; // utilisateur de la base de données
-    $_ENV["password"] = ""; // mot de passe de l'utilisateur de la base de données
-    $_ENV["dsn"] = "mysql:host=localhost;dbname=tableau_bord;port=3306"; // data source name
+    $_ENV["username"] = "data_user"; // utilisateur de la base de données
+    $_ENV["password"] = "datapswd222"; // mot de passe de l'utilisateur de la base de données
+    $_ENV["dsn"] = "mysql:host=172.30.0.65;dbname=tableau_bord;port=3306"; // data source name
     $_ENV["options"] = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'') // option pour le driver PDO : UTF8 pour gérer les accents
 ?>
