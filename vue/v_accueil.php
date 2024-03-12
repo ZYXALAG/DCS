237447d1823d0fd957d612fe075b6845ec633cce
Tomy Rulliat <tomy.rulliat@clsi.fr>
2024-03-12 11:09:59 +0100

    accueil

--- vue/v_accueil.php
+++ vue/v_accueil.php
@@ -1,5 +1,10 @@
-<h1>
-Bienvenue sur le tableau de bord.
+
+<div class="container"><h1>
+Bienvenue aux DCS IT GAMES !
 </h1>
 <br><br>
-<h2></h2>
+<h4>Découvrez notre application simple et intuitive pour consulter les données de facturation. Avec trois écrans clairs et efficaces, vous pourrez explorer les coûts des applications, l'évolution des montants facturés et comparer les volumes sur une période donnée.</h4>
+<br>
+<h4>
+Plongez dans l'univers de la facturation avec DCS IT GAMES et explorez les données financières d'une manière simple et accessible.</h4>
+</div>
--- vue/v_menu.php
+++ vue/v_menu.php
@@ -6,13 +6,13 @@
           <a class="nav-link" href="./?action=accueil">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./?action=appligc">Top des coûts d'application par Grand Clients</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./?action=evolutionMensuelle">Evolution Mensuelle</a>
+          <a class="nav-link" href="./?action=appligc">Coûts d'application par Grand Clients</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./?action=evolVolumeFacture">Evolution des volumes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./?action=evolutionMensuelle">Evolution Mensuelle</a>
         </li>
       </ul>
     </div>
